ccc6683d207897b99e20ada76b920886f1d231cd
jovkris <jovkris0605@gmail.com>
2022-02-07 13:04:30 +0100

    src za img promenjen

--- 29_PROJEKAT/public/ui.js
+++ 29_PROJEKAT/public/ui.js
@@ -48,7 +48,7 @@ class ChatUI{
         let id = data.id;
         data = data.data();
         let li = document.createElement(`li`);
-        li.innerHTML = `<span id="usernameSPN">${data.username} :</span> <span id="msgSPN">${data.message}</span> <span id="dateSPN">${this.formatDate(data)}</span><img src="./img/trash.png">`;
+        li.innerHTML = `<span id="usernameSPN">${data.username} :</span> <span id="msgSPN">${data.message}</span> <span id="dateSPN">${this.formatDate(data)}</span><img src="../img/trash.png">`;
         li.setAttribute(`id`, `${id}`);
 
         console.log(data.username);
